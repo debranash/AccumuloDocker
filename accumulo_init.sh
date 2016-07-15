39c1f21dbe1fd99935d77e102923ffbdc67a9015
Gianluca Scacco <gianluca.scacco@gmail.com>
2016-07-15 10:39:09 +0200

    Modificati l'indirizzo di ascolto ed il nome dell'istanza

--- accumulo_init.sh
+++ accumulo_init.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 /root/installs/accumulo-1.6.4/bin/accumulo init << EOF
-myinstance
+accumulo-instance
 password
 password
 EOF
-sed -i 's/localhost/localhost\n'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/masters
-sed -i 's/localhost/localhost\n'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/slaves
+sed -i 's/localhost/'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/masters
+sed -i 's/localhost/'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/slaves
+sed -i 's/localhost/'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/gc
+sed -i 's/localhost/'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/tracers
+sed -i 's/localhost/'"$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"'/g' ~/installs/accumulo-1.6.4/conf/monitor
